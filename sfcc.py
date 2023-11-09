6c0649100fb2dcc53feb972ff5ab2e4dd680a783
consultor <consultor@kali.entelgy>
2023-11-09 00:14:00 -0300

    Multiple function changed

--- sfcc.py
+++ sfcc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import signal, sys
+import signal, sys, time
 from os import system
 
 # Exit
@@ -14,32 +14,32 @@ def banner():
     font = """
      _______ _______ _______ _______      ______ _______ __   _
      |______ |______ |______ |       ___ |  ____ |______ | \  |
-     ______| |       |       |_____      |_____| |______ |  \_| """
+     ______| |       |       |_____      |_____| |______ |  \_| by H4RRIZN"""
     print(font)
 
 # Main
 
 if __name__ == '__main__':
-    banner()
     while True:
+        banner()
         eval = input("\n¿ Incluir eval() ? (y/n)\n")
         if eval == "y":
             payload = input("\nIngresa tu payload: \n")
-            results = []
-            for char in payload:
-                unicode_value = ord(char)
-                results.append(unicode_value)
-            results.sort()
-            formatted_results = ",".join(str(value) for value in results)
-            print("\neval(String.fromCharCode("+formatted_results+"))")
+            unicode_conv = [ord(char) for char in payload]
+            unicode_string = ",".join([str(code) for code in unicode_conv])
+            eval_string = f"eval(String.fromCharCode({unicode_string}));"
+            print("\nCharcode Generado:\n")
+            print(eval_string)
             break
         if eval == "n":
             payload = input("\nIngresa tu payload: \n")
-            results = []
-            for char in payload:
-                unicode_value = ord(char)
-                results.append(unicode_value)
-            results.sort()
-            formatted_results = ",".join(str(value) for value in results)
-            print("\nString.fromCharCode("+formatted_results+"))")
-            break
+            unicode_conv = [ord(char) for char in payload]
+            unicode_string = ",".join([str(code) for code in unicode_conv])
+            eval_string = f"String.fromCharCode({unicode_string});"
+            print("\nCharcode Generado:\n")
+            print(eval_string)
+        else:
+            print("[!] Opcion invalida [!]")
+            time.sleep(2)
+            system('clear')
+
